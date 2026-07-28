b5490e499fd65f6b15b972570d9758a7bdc425c1
Furkan <xpodiumyours@gmail.com>
2026-07-28 21:10:19 +0300

    Revert "fix(sentry): filter out local Chrome DevTools WipError debugger logs"
    This reverts commit 23005eeeb28235eb235642f29c8ff005f431e89c.

--- lib/main.dart
+++ lib/main.dart
@@ -26,18 +26,6 @@ Future<void> main() async {
   await SentryFlutter.init((options) {
     options.dsn = const String.fromEnvironment('SENTRY_DSN');
     options.tracesSampleRate = 0.2;
-    options.beforeSend = (event, {hint}) {
-      final throwableStr = event.throwable?.toString() ?? '';
-      final messageStr = event.message?.formatted ?? '';
-      if (throwableStr.contains('WipError') ||
-          throwableStr.contains('dartDevEmbedder') ||
-          messageStr.contains('WipError') ||
-          messageStr.contains('dartDevEmbedder') ||
-          messageStr.contains('AppInspector')) {
-        return null;
-      }
-      return event;
-    };
   }, appRunner: () => runApp(const VixRexApp()));
 }
 
