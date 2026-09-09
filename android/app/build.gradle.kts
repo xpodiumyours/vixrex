c2011a40412c4495dfe577656b9712e6a3b752de
Furkan <xpodiumyours@gmail.com>
2026-09-09 22:31:18 +0300

    fix(android): v1+v2 APK imzasi + minSdk 24

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -41,17 +41,16 @@ android {
                 storeFile = file(storePath)
                 storePassword = storePasswordValue
                 keyAlias = keyAliasValue
-            // Eski/kati yukleyiciler icin v1 (JAR) imzasi da uretilsin.
-            enableV1Signing = true
-            enableV2Signing = true
                 keyPassword = keyPasswordValue
+                enableV1Signing = true
+                enableV2Signing = true
             }
         }
     }
 
     defaultConfig {
         applicationId = "com.xpodiumyours.vixrex"
-        minSdk = flutter.minSdkVersion
+        minSdk = 24
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
